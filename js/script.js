c7d999c2db1065ba563d4e2c17238d30d07f92ea
Aline Pereira <alinepereiracolli@gmail.com>
2022-07-28 15:33:51 -0300

    Aplicação de JS e Jquery

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
+/************** JQUERY NOS BOTÕES **************/
+
+$(document).ready(function(){
+
+    $(".card-buy").click(function(){
+        alert('Ops, este produto já acabou :(');
+
+        $(this).text('Indisponível');
+    })
+
+    $('.botao-banner').click(function(){
+        alert('Ops, este produto já acabou :(');
+
+        $(this).text('Indisponível');
+    })
+})
+
+/************** MENU **************/
+
 const btnMobile = document.getElementById('btn-mobile')
 
 function mostrarMenu(){
@@ -6,3 +25,24 @@ function mostrarMenu(){
 }
 
 btnMobile.addEventListener('click', mostrarMenu)
+
+/************** ZOOM IMAGEM BANNER **************/
+
+const banner = document.getElementById('imagem-banner');
+const imgBanner = document.getElementById('img-banner');
+
+banner.addEventListener('mousemove', (e) =>{
+    const x = e.clientX - e.target.offsetLeft;
+    const y = e.clientY - e.target.offsetTop;
+
+    console.log(x,y);
+
+    imgBanner.style.transformOrigin = `${x}px ${y}px`;
+    imgBanner.style.transform = 'scale(2)';
+});
+
+banner.addEventListener('mouseleave', () =>{
+    imgBanner.style.transformOrigin = 'center center';
+    imgBanner.style.transform = 'scale(1)';
+});
+
